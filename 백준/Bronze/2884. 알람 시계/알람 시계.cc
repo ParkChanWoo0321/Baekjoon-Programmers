3e0b65a9d92012ad13deb3c28540d19fa8d9e541
ChanWoo <op9563_@naver.com>
2025-08-30 21:44:58 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int h,m;
+    cin>>h>>m;
+   if(h==0&&m<45)
+   {
+       cout<<"23 "<<60-(45-m)<<endl;        
+   }
+   else
+   {
+       if(m<45)
+           cout<<h-1<<" "<<60-(45-m)<<endl;
+       else
+           cout<<h<<" "<<m-45<<endl;
+   }
+}
