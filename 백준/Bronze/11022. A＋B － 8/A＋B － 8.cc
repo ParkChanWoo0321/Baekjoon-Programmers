7552386d7173ac10fbd46a8f3577d1bf7593105a
ChanWoo <op9563_@naver.com>
2025-10-13 15:32:15 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int a;
+    int b[256] = {0};
+    int c[256] = { 0 };
+    cin >> a;
+    for (int i = 0; i < a; i++)
+    {
+        cin >> b[i] >> c[i];
+    }
+    for (int j = 0; j < a; j++)
+    {
+        cout << "Case #" << j + 1 << ": " << b[j]<<" + "<<c[j]<<" = "<<b[j]+c[j]<< endl;
+    }
+}
