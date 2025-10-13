c09b3badb09b7d2a3723e8a5614dda26284874e1
ChanWoo <op9563_@naver.com>
2025-10-13 15:35:52 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int a;
+    cin>>a;
+    for(int i=1;i<=a;i++)
+    {
+        for(int k=0;k<a-i;k++)
+        {
+            cout<<" ";
+        }
+        for(int j=0;j<i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
