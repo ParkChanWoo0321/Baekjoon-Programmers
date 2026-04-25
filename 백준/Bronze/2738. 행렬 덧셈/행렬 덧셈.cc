28440e8284f3f21ab8df7418445ea02fc7e026d2
ChanWoo <op9563_@naver.com>
2026-04-25 20:16:34 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N, M;
+    cin >> N >> M;
+    int A[100][100];
+    int B[100][100];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> A[i][j];
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> B[i][j];
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << A[i][j] + B[i][j];
+            if (j != M - 1) cout << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
